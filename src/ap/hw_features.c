f8afa0a4137ccee67d6bf2fe54b36479defbded2
Xinyue Ling <quic_xinyling@quicinc.com>
2023-06-07 16:13:34 +0800

    Determine current hw mode before channel switch
    There are two hw modes (5 GHz and 6 GHz) with HOSTAPD_MODE_IEEE80211A
    and the current hw mode may be wrong after one channel switch to 6 GHz.
    This will cause hostapd_set_freq_params() to return -1 when saving
    previous state and the second channel switch to fail. Fix this by adding
    hostapd_determine_mode() before every channel switch.
    
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>
    CRs-Fixed: 3930920
    Git-repo: git://w1.fi/hostap.git
    Git-commit: 3a995cb5a9f5748f49ed3d86615e4e3619e9cc7a
    Change-Id: I3a73c0361c9f834ab8b3bfa5192834f1ba98dc4b

--- src/ap/hw_features.c
+++ src/ap/hw_features.c
@@ -1032,7 +1032,7 @@ static bool skip_mode(struct hostapd_iface *iface,
 }
 
 
-static void hostapd_determine_mode(struct hostapd_iface *iface)
+void hostapd_determine_mode(struct hostapd_iface *iface)
 {
 	int i;
 	enum hostapd_hw_mode target_mode;
